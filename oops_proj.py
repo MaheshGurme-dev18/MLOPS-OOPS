20b79dfec5a8923f035e666606e81f5c3e4b4fb9
Mahesh Gurme <maheshgurme@example.com>
2025-12-12 14:37:41 +0530

    covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -2,7 +2,7 @@ class chatbook:
     def __init__(self):
         self.username= ''
         self.password=''
-        self.loggein= False
+        self.loggedin= False
         self.menu()
 
 
@@ -12,15 +12,17 @@ class chatbook:
                            2. Press 2 to signin
                            3. Press 3 to write a post
                            4. Press 4 to message a friend
-                           5. Press any other key to exit""")
+                           5. Press any other key to exit 
+                           
+                           ->""")
         if user_input =="1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else:
             exit()
 
@@ -32,6 +34,7 @@ class chatbook:
         print("you have signup successfully !!")
         print("\n")
         self.menu()
+
     def signin(self):
         if self.username == '' and self.password=='':
             print("Please signup first by pressing 1 in the main menu")
@@ -45,6 +48,31 @@ class chatbook:
                 print("print input correct credentials..")
         print("\n")
         self.menu()
+
+    def my_post(self):
+        if self.loggedin==True:
+             txt = input("Enter your message here->")
+             print(f"following content has been posted -> {txt}")
+        else:
+            print("you need to signin first to post something...")
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here->")
+            frnd=input("whom to send the message?->")
+            print(f"your message has been sent to {frnd}")
+        else:
+            print("you need to sign in first to post something...")
+        print("\n")
+        self.menu()
+
+       
+
+
+
+
 
 obj = chatbook()
 
